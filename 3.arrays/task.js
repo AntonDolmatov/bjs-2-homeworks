d9fb2cbd2ead600ef02bb62ac245606031ace0f6
Anton Dolmatov <tosha.dolmatow@yandex.ru>
2024-02-27 13:32:43 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
+"use strict";
+
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+
+	return arr1.every((value, index) => value === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const result = users.filter(users => users.gender === gender).map(users => users.age).reduce((acc, item, index, arr) => acc + item / arr.length, 0);
+  return result;
 }
