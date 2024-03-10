486ed601cb31b929f451c1ac493ed235e05823cb
Anton Dolmatov <tosha.dolmatow@yandex.ru>
2024-03-10 19:27:25 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,52 @@
-﻿
+﻿"use strict"
+
+function parseCount(value) {
+	let result = Number.parseFloat(value);
+	if (Number.isNaN(result)) {
+		throw new Error("Невалидное значение");
+	}
+	return result;
+}
+
+function validateCount(value) {
+	try {
+		return parseCount(value);
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(side1, side2, side3) {
+		if ((side1 + side2 <= side3) || (side1 + side3 <= side2) || (side2 + side3 <= side1)) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		}
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+	}
+
+	get perimeter() {
+		return (this.side1 + this.side2 + this.side3);
+	}
+
+	get area() {
+		let p = this.perimeter / 2;
+		return +Math.sqrt(p * (p - this.side1) * (p - this.side2) * (p - this.side3)).toFixed(3);
+	}
+}
+
+function getTriangle(side1, side2, side3) {
+	try {
+		return new Triangle(side1, side2, side3);
+	} catch (error) {
+		return {
+			get area() {
+				return 'Ошибка! Треугольник не существует';
+			},
+			get perimeter() {
+				return 'Ошибка! Треугольник не существует';
+			}
+		}
+	}
+}
